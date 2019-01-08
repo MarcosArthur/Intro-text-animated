cd6cd47f636e7e0a7ad33b5fbd28b11efa245498
MarcosArthur <arturcruz0@gmail.com>
2019-01-08 21:53:59 -0200

    Melhorias usando api visible

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 let text = document.querySelector('.text-intro__animated');
 let subTitle = document.querySelector('.text-intro__sub-title');
 window.addEventListener('load', () => {
-  text.classList.add('load');
-  subTitle.classList.add('load');
+  document.addEventListener('visibilitychange', () => {
+     text.classList.add('load');
+     subTitle.classList.add('load');
+  })
+  
 })
