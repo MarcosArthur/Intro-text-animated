5c66802b7fad1bb3482dc6622984d7e63756f8ff
MarcosArthur <arturcruz0@gmail.com>
2019-01-08 21:37:06 -0200

    Ajustes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-let text =   document.querySelector('.text-intro__animated');
-
+let text = document.querySelector('.text-intro__animated');
+let subTitle = document.querySelector('.text-intro__sub-title');
 window.addEventListener('load', () => {
   text.classList.add('load');
+  subTitle.classList.add('load');
 })
