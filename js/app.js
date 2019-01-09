8e6bb40fe4eb8c55d7532a7f9ea69fadcb0c6d94
MarcosArthur <arturcruz0@gmail.com>
2019-01-08 22:13:44 -0200

    V1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 let text = document.querySelector('.text-intro__animated');
 let subTitle = document.querySelector('.text-intro__sub-title');
+
 window.addEventListener('load', () => {
+
   document.addEventListener('visibilitychange', () => {
      text.classList.add('load');
      subTitle.classList.add('load');
   })
-  
+
+  visible();  
 })
+
+function visible() {
+  if (document.visibilityState == "visible") {
+    text.classList.add('load');
+    subTitle.classList.add('load');
+  } 
+  
+}
